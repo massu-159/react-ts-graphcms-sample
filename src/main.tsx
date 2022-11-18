51d7d8d27cc7673db3be48b8c974201b319724e5
massu-159 <magjtwjp@gmail.com>
2022-11-18 22:34:12 +0900

    add ApolloClient

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: "https://api-ap-northeast-1.hygraph.com/v2/clamij6mg2yro01tc78aubq69/master",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 )
