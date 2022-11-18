01a9c9c0ca7d0afb4b1ee8397c4079bb6f186f3d
massu-159 <magjtwjp@gmail.com>
2022-11-18 23:27:02 +0900

    connect graphCMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
 import './App.css'
+import { gql, useQuery } from '@apollo/client'
+import { Dog } from './interfaces/Dog';
 
-function App() {
-  const [count, setCount] = useState(0)
+const DOGS = gql`
+  query Dogs {
+    dogs {
+      description
+      id
+      name
+      thumbnail {
+        url
+      }
+    }
+  }
+`;
 
+function App(): JSX.Element {
+  const { data, loading, error } = useQuery(DOGS);
+
+  if (loading) { return <div>ロード中・・・</div> };
+  if (error) { return <div>エラー：{error.message}</div> };
+  
   return (
     <div className="App">
-      
+      <h1>Dogs Image App</h1>
+      <div className="dogsContainer">
+        {data.dogs.map((dog: Dog) => (
+          <div key={dog.id}>
+            <div className="dogCard">
+              <img src={dog.thumbnail.url} alt=""/>
+              <p>{ dog.name }</p>
+              <p>{ dog.description }</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
